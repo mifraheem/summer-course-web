70a34342e60d4151766129e12c39745c20cd915e
M Ifraheem <mifrahim017@gmail.com>
2025-08-06 12:50:22 +0500

    operators added

---
+++ Python/lec1.py
@@ -0,0 +1,13 @@
+stdName = 'Mehnaz'
+stdRollNo = 6
+stdCgpa = 3.2
+isGraduated = False
+print(stdName)
+
+
+print(stdName)
+print(stdRollNo)
+print(stdCgpa)
+print(isGraduated)
+
+print(stdName, stdRollNo, stdCgpa, isGraduated)
