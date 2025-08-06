8faf49bd2c2b6ea97cbc3945d88924e3159fb1df
M Ifraheem <mifrahim017@gmail.com>
2025-08-06 13:23:20 +0500

    python type casting

---
+++ Python/studentData.py
@@ -0,0 +1,13 @@
+print("Welcome to the Student Data Module")
+
+name = input("Enter your name: ")
+
+print(name,":" ,type(name))
+
+age = input("Enter your age: ")
+print(age,":" ,type(age))
+
+age = int(age)
+print(age,":" ,type(age))
+
+print(age > 40)
