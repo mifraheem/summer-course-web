564baff112250b164c1168e5c91d3b1c5b396b63
M Ifraheem <mifrahim017@gmail.com>
2025-08-13 13:19:19 +0500

    python functions

---
+++ Python/functions/fun1.py
@@ -0,0 +1,33 @@
+print("Functions in Python")
+
+def myIntro():
+  print("hey, i am sudais")
+
+
+a = 4
+b = 10
+c = a+b
+myIntro()
+
+
+#
+
+
+
+
+
+
+
+
+myIntro()
+
+
+
+
+
+
+
+
+
+
+myIntro()
---
+++ Python/functions/fun2.py
@@ -0,0 +1,20 @@
+# def myData():
+#   a = 4
+#   b = 10
+#   return a + b
+
+
+# print(myData())
+# # res = myData()
+# # print(res)
+
+
+
+
+
+def userData():
+  value = input("Enter a value: ")
+  return value
+
+data = userData()
+print(data)
