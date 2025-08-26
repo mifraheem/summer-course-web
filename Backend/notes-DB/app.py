ea3351fe086cf73a9e7f92ce6355baa371e5b32e
M Ifraheem <mifrahim017@gmail.com>
2025-08-26 12:45:02 +0500

    notes project with db

---
+++ Backend/notes-DB/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notes.sqlite"
+
+db = SQLAlchemy(app)
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=True)
+
+
+@app.route('/')
+def notes():
+    allNotes = Note.query.all()
+    return render_template("index.html", allNotes=allNotes)
+
+
+@app.route("/create-note", methods=['GET', 'POST'])
+def addNote():
+    if request.method == 'POST':
+        print("hey, user just posted a form...!")
+        titleName = request.form["title"]
+        contentData = request.form["content"]
+
+        new_note = Note(title=titleName, content=contentData)
+        db.session.add(new_note)
+        db.session.commit()
+
+    return render_template("add.html")
+
+@app.route("/delete/<id>")
+def deleteNote(id):
+    note = Note.query.get(id)
+    if note:
+        db.session.delete(note)
+        db.session.commit()
+    return redirect("/")
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
